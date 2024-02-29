8fe2d871e583a4ec475acfe90d8e92e98b966491
artemplohenko@gmail.com <artemplohenko@gmail.com>
2024-02-29 20:07:24 +0200

    added unmountOnExit, onEnter and onExited

--- src/App.js
+++ src/App.js
@@ -20,7 +20,13 @@ const Modal = (props) => {
   };
 
   return (
-    <Transition in={props.show} timeout={duration}>
+    <Transition
+      in={props.show}
+      timeout={duration}
+      unmountOnExit
+      onEnter={() => props.setShowTrigger(false)}
+      onExited={() => props.setShowTrigger(true)}
+    >
       {(state) => (
         <div
           className="modal mt-5 d-block"
@@ -61,13 +67,16 @@ const Modal = (props) => {
 
 function App() {
   const [showModal, setShowModal] = useState(false);
+  const [showTrigger, setShowTrigger] = useState(true);
 
   return (
     <Container>
-      <Modal show={showModal} onClose={setShowModal} />
-      <button type="button" className="btn btn-warning mt-5" onClick={() => setShowModal(true)}>
-        Open Modal
-      </button>
+      <Modal show={showModal} onClose={setShowModal} setShowTrigger={setShowTrigger} />
+      {showTrigger ? (
+        <button type="button" className="btn btn-warning mt-5" onClick={() => setShowModal(true)}>
+          Open Modal
+        </button>
+      ) : null}
     </Container>
   );
 }
